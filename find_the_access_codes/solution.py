9f521f78d03830ccf09ce49640aa854d21d1dd02
Samy Amraoui <sametzy@gmail.com>
2021-03-06 11:35:50 +0000

    Implement naive solution

--- find_the_access_codes/solution.py
+++ find_the_access_codes/solution.py
@@ -1,2 +1,11 @@
 def solution(l):
-    return 0
+    if (len(l) < 3):
+        return 0
+    triple_count = 0
+    for i in range(len(l) - 2):
+        for j in range(i + 1, len(l) - 1):
+            for k in range(j + 1, len(l)):
+                if ((l[j] % l[i] == 0) and (l[k] % l[j] == 0)):
+                    triple_count += 1
+    return triple_count
+
