ffe90d94063eb8b7b3016042609657d65760cdee
Samy Amraoui <sametzy@gmail.com>
2021-03-06 17:10:41 +0000

    Implement optimal solution

--- fuel_injection_perfection/test.py
+++ fuel_injection_perfection/test.py
@@ -2,12 +2,21 @@ import unittest
 from solution import solution
 
 
-class TestFuelInjectionPerfection(unittest.TestCase):
+class TestFuelInjectionPerfection(unittest.TestCase):    
     def test_1(self):
-        self.assertEqual(solution(4), 2)
+        self.assertEqual(solution('3'), 2)
 
     def test_2(self):
-        self.assertEqual(solution(15), 5)
+        self.assertEqual(solution('2'), 1)
+    
+    def test_3(self):
+        self.assertEqual(solution('4'), 2)
+
+    def test_4(self):
+        self.assertEqual(solution('15'), 5)
+    
+    def test_5(self):
+        self.assertEqual(solution('34'), 6)
 
 
 if __name__ == '__main__':
